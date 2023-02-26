da9fe48d4d349361ffc8fc86d048a514865764c2
taehee <jangth0655@gmail.com>
2023-02-26 12:05:24 +0900

    feat : 금결원 사용자 인증 test

--- src/pages/login/Certification.tsx
+++ src/pages/login/Certification.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../../components/Loading';
@@ -15,8 +16,8 @@ const Certification = () => {
   const { mutation, data, error, loading } = useMutation('/openBank/token');
   const userInfoDispatch = useAppDispatch();
 
+  const code = search.split('&')[0].split('=')[1];
   const handleOpenBankCode = () => {
-    const code = search.split('&')[0].split('=')[1];
     if (!code) return setMessage('올바르지 않은 인증입니다.');
     mutation({
       code,
@@ -25,19 +26,22 @@ const Certification = () => {
   };
 
   useEffect(() => {
-    /* if (!search) {
-      navigate('/');
-    } */
-    console.log('useEffect', data);
-    handleOpenBankCode();
+    console.log('mount');
+    code && handleOpenBankCode();
   }, []);
+
+  useEffect(() => {
+    console.log('test data', data);
+  }, [data]);
 
   return message || error ? (
     <div className="p-5">
       <span className="text-xl text-red-400 font-bold">{message || error}</span>
     </div>
   ) : (
-    <></>
+    <>
+      <button className="border-2 px-10 py-2">인증</button>
+    </>
   );
 };
 export default Certification;
